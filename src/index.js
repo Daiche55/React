ce82ef2340f312b7a8d1d131669754569cb20f62
藤田大地 <fujita@fujitadaichinoMacBook-Pro.local>
2020-08-21 23:34:56 +0900

    Todo 初め

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class ToDoList extends React.Component {
+  render() {
+    return (
+      <div>{this.}</div>
+    )
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <ToDoList />,
+  document.getElementById('root')
+);
