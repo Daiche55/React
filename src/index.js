4062dd20f064c98552e5a93adce6c0a87c3b807c
藤田大地 <fujita@fujitadaichinoMacBook-Pro.local>
2020-08-21 23:58:25 +0900

    とりあえずコミット

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
 class ToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      whatToDo: [{
+        tasks: Array(3).fill(null),
+      }]
+    };
+  };
+  displayTasks(i) {
+    return (
+      <Task />
+    )
+  }
   render() {
     return (
-      <div>{this.}</div>
+      <div>{this.displayTasks(1)}</div>
     )
   }
 }
-
+function Task(props) {
+  return (
+    <div>aa</div>
+  )
+}
 // ========================================
-
 ReactDOM.render(
   <ToDoList />,
   document.getElementById('root')
