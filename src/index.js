ccabcbadec6ee9c930742d00c47525cad68f4793
藤田大地 <fujita@fujitadaichinoMacBook-Pro.local>
2020-08-22 23:20:33 +0900

    editモード作成

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import './index.css';
 
 class List extends React.Component {
@@ -12,10 +12,16 @@ class List extends React.Component {
     };
   };
 
+  clearFields = (e) => {
+    e.target.elements['title'].value = '';
+    e.target.elements['description'].value = '';
+  }
+
   submitToDo = (e) => {
     e.preventDefault();
     const title = e.target.elements['title'].value;
     const description = e.target.elements['description'].value;
+
     this.setState(
       {
         toDoList: this.state.toDoList.concat({
@@ -24,14 +30,43 @@ class List extends React.Component {
         })
       }
     )
-    e.target.elements['title'].value = '';
-    e.target.elements['description'].value = '';
+    this.clearFields(e);
+  }
+
+  editToDo = (e, index) => {
+    e.preventDefault();
+    const title = e.target.elements['title'].value;
+    const description = e.target.elements['description'].value;
+    const arg_index = index;
+
+    this.state.toDoList.map((todo, index) => {
+      if (index === arg_index) {
+        this.state.toDoList[index].title = title;
+        this.state.toDoList[index].description = description;
+      };
+    });
+
+    this.setState({
+      toDoList: this.state.toDoList
+    });
+    this.clearFields(e);
+  }
+
+  deleteToDo = (index) => {
+    this.state.toDoList.splice(index, 1);
+    this.setState({toDoList: this.state.toDoList});
   }
 
   eachToDo() {
     return (
       this.state.toDoList.map((todo, index) => (
-        <ToDo index={index} title={todo.title} description={todo.description} />
+        <ToDo index={index}
+              key={todo.index}
+              title={todo.title}
+              description={todo.description}
+              deleteToDo={this.deleteToDo}
+              editToDo={this.editToDo}
+        />
       ))
     )
   }
@@ -39,10 +74,11 @@ class List extends React.Component {
   render() {
     return (
       <div>
+        <h2>君にとっての最高のToDoリストを！</h2>
         <form onSubmit={this.submitToDo}>
           <input type="text" name="title" placeholde="title" />
           <input type="text" name="description" placeholde="title" />
-          <input type="submit" value="Submit"></input>
+          <input type="submit" value="送信"></input>
         </form>
         {this.eachToDo()}
       </div>
@@ -50,16 +86,56 @@ class List extends React.Component {
   }
 }
 
-function ToDo(props) {
-  return (
-    <div>
-      <h1>タスクNo. {props.index + 1}</h1>
-      <h2>今日すること:</h2><br></br>
-      {props.title}<br></br>
-      <h2>詳しい内容:</h2><br></br>
-      {props.description}<br></br>
-    </div>
-  );
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editToDo: false,
+    };
+  };
+
+  eachToDoEdit = () => {
+    this.setState({
+      editToDo: !this.state.editToDo
+    });
+  }
+
+  render(props) {
+    var props = this.props;
+    return (
+      <div key={props.key}>
+        <h1>タスクNo. {props.index+1}</h1>
+        <h2>今日すること:</h2><br></br>
+        {props.title}<br></br>
+        <h2>詳しい内容:</h2><br></br>
+        {props.description}<br></br>
+        <button onClick={() => props.deleteToDo(props.index)}>Delete</button>
+        {this.state.editToDo ?
+          <div>
+            <p><Form index={props.index} editToDo={props.editToDo}/></p>
+            <button onClick={this.eachToDoEdit}>戻る</button>
+          </div>
+          :
+          <button onClick={this.eachToDoEdit}>Edit</button>
+        }
+      </div>
+    );
+  }
+}
+
+class Form extends React.Component {
+  render(props) {
+    var props = this.props;
+    return (
+      <div>
+        <form onSubmit={(e) => props.editToDo(e, props.index)}>
+          <input type="text" name="title" placeholde="title" />
+          <input type="text" name="description" placeholde="title" />
+          <input type="submit" value="編集する"></input>
+        </form>
+      </div>
+    )
+  }
 }
 
 // ========================================
