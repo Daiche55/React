41c1434ca942fe9b77c4b881dfa5f054fdd1fc3f
藤田大地 <fujita@fujitadaichinoMacBook-Pro.local>
2020-08-23 13:59:18 +0900

    未完・完了タスクとりあえず機能は作成

--- src/index.js
+++ src/index.js
@@ -13,8 +13,7 @@ class List extends React.Component {
   };
 
   clearFields = (e) => {
-    e.target.elements['title'].value = '';
-    e.target.elements['description'].value = '';
+
   }
 
   submitToDo = (e) => {
@@ -57,6 +56,19 @@ class List extends React.Component {
     this.setState({toDoList: this.state.toDoList});
   }
 
+  taskFinished = (index) => {
+    const arg_index = index;
+    this.state.toDoList.map((todo, index) => {
+      if (index === arg_index) {
+        this.state.toDoList[index].taskFinished = !this.state.toDoList[index].taskFinished;
+      };
+    });
+
+    this.setState({
+      toDoList: this.state.toDoList
+    });
+  };
+
   eachToDo() {
     return (
       this.state.toDoList.map((todo, index) => (
@@ -66,7 +78,40 @@ class List extends React.Component {
               description={todo.description}
               deleteToDo={this.deleteToDo}
               editToDo={this.editToDo}
+              taskFinished={this.taskFinished}
         />
+      ))
+    )
+  }
+
+  todo_done_lists() {
+    return (
+      this.state.toDoList.map((todo, index) => (
+        (todo.taskFinished === true ?
+          <ToDo index={index}
+                key={todo.index}
+                title={todo.title}
+                description={todo.description}
+          />
+        :
+        <p>完了タスクはありません</p>
+        )
+      ))
+    )
+  }
+
+  todo_not_yet_lists() {
+    return (
+      this.state.toDoList.map((todo, index) => (
+        (todo.taskFinished !== true ?
+          <ToDo index={index}
+            key={todo.index}
+            title={todo.title}
+            description={todo.description}
+          />
+          :
+          <p>未完タスクはありません</p>
+        )
       ))
     )
   }
@@ -74,13 +119,29 @@ class List extends React.Component {
   render() {
     return (
       <div>
-        <h2>君にとっての最高のToDoリストを！</h2>
-        <form onSubmit={this.submitToDo}>
-          <input type="text" name="title" placeholde="title" />
-          <input type="text" name="description" placeholde="title" />
-          <input type="submit" value="送信"></input>
-        </form>
-        {this.eachToDo()}
+        <div className="todo_form">
+          <h4>君にとっての最高のToDoリストを！</h4>
+          <form onSubmit={this.submitToDo}>
+            <input type="text" name="title" placeholde="title" />
+            <input type="text" name="description" placeholde="description" />
+            <input type="submit" value="送信"></input>
+          </form>
+        </div>
+        <div className="todos">
+          <div className="each_todos">
+            {this.eachToDo()}
+          </div>
+          <div className="todo_done_not_yet_lists">
+            <div className="todo_done_lists">
+              <h3>完了タスク</h3>
+              {this.todo_done_lists()}
+            </div>
+            <div className="todo_not_yet_lists">
+              <h3>未完タスク</h3>
+              {this.todo_not_yet_lists()}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
@@ -103,21 +164,25 @@ class ToDo extends React.Component {
   render(props) {
     var props = this.props;
     return (
-      <div key={props.key}>
-        <h1>タスクNo. {props.index+1}</h1>
-        <h2>今日すること:</h2><br></br>
-        {props.title}<br></br>
-        <h2>詳しい内容:</h2><br></br>
-        {props.description}<br></br>
-        <button onClick={() => props.deleteToDo(props.index)}>Delete</button>
-        {this.state.editToDo ?
-          <div>
-            <p><Form index={props.index} editToDo={props.editToDo}/></p>
-            <button onClick={this.eachToDoEdit}>戻る</button>
-          </div>
-          :
-          <button onClick={this.eachToDoEdit}>Edit</button>
-        }
+      <div>
+        <div className="each_todos" key={props.key}>
+          <h3>タスクNo. {props.index + 1}</h3>
+          <h3>今日すること:</h3><br></br>
+          <h2 className="todo_content">{props.title}</h2><br></br>
+          <h3>詳しい内容:</h3><br></br>
+          <h2 className="todo_content">{props.description}</h2><br></br>
+
+          <button onClick={() => props.deleteToDo(props.index)}>削除する</button>
+          {this.state.editToDo ?
+            <div>
+              <p><Form index={props.index} editToDo={props.editToDo} /></p>
+              <button onClick={this.eachToDoEdit}>戻る</button>
+            </div>
+            :
+            <button onClick={this.eachToDoEdit}>編集する</button>
+          }
+          <button onClick={() => props.taskFinished(props.index)}>タスク完了！</button>
+        </div>
       </div>
     );
   }
